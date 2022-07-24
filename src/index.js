9871fd8db4112eada48e92b449a2b7241c344c79
Nadi Hameza <nadya1gam@gmail.com>
2022-07-24 21:40:50 +0300

    towel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let massive = [];
+  if (matrix === undefined) {
+    return massive;
+  }
+  else {
+  for (let i = 1; i <matrix.length; i+= 2) {
+   matrix[i].reverse();  
+  }
+  
+  for (let j = 0; j < matrix.length; j++) {
+    let qwert = matrix[j];
+    massive = massive.concat(qwert);
+  }
+
+  return massive;
+}
 }
